a33e6d72da3b6a41bbbd43f9b363b063b3cf847d
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2019-08-31 14:30:58 +0300

    Parse moves; simplify piece parsing

--- src/bin/historian.rs
+++ src/bin/historian.rs
@@ -1,4 +1,5 @@
 use rbc::api;
+use rbc::game::{STARTING_FEN, Move};
 
 fn main() {
     env_logger::init();
@@ -12,6 +13,17 @@ fn main() {
                     "{}: {:>25} - {:25} {:?} {:12}  {} moves",
                     game_id,
                     h.white_name, h.black_name, h.winner_color, h.win_reason, h.moves.len());
+                if let Some(m) = h.moves.first() {
+                    assert_eq!(m.fen_before, STARTING_FEN);
+                }
+                for m in &h.moves {
+                    if let Some(q) = &m.requested_move {
+                        Move::from_uci(q);
+                    }
+                    if let Some(q) = &m.taken_move {
+                        Move::from_uci(q);
+                    }
+                }
             }
             Err(api::Error::HttpError(404)) => {
                 println!("{}: game does not exist", game_id);
--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,8 @@
 use serde::Deserialize;
 
-#[derive(Clone, Copy)]
+pub const STARTING_FEN: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum PieceKind {
     Pawn,
     Knight,
@@ -8,6 +10,31 @@ pub enum PieceKind {
     Rook,
     Queen,
     King,
+}
+
+impl PieceKind {
+    fn from_char(c: char) -> PieceKind {
+        match c {
+            'p' => PieceKind::Pawn,
+            'n' => PieceKind::Knight,
+            'b' => PieceKind::Bishop,
+            'r' => PieceKind::Rook,
+            'q' => PieceKind::Queen,
+            'k' => PieceKind::King,
+            _ => panic!("{:?}", c),
+        }
+    }
+
+    fn to_char(self) -> char {
+        match self {
+            PieceKind::Pawn => 'p',
+            PieceKind::Knight => 'n',
+            PieceKind::Bishop => 'b',
+            PieceKind::Rook => 'r',
+            PieceKind::Queen => 'q',
+            PieceKind::King => 'k',
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -28,37 +55,23 @@ pub struct Piece {
 
 impl Piece {
     pub fn from_char(c: char) -> Piece {
-        match c {
-            'P' => Piece { color: Color::White, kind: PieceKind::Pawn },
-            'N' => Piece { color: Color::White, kind: PieceKind::Knight },
-            'B' => Piece { color: Color::White, kind: PieceKind::Bishop },
-            'R' => Piece { color: Color::White, kind: PieceKind::Rook },
-            'Q' => Piece { color: Color::White, kind: PieceKind::Queen },
-            'K' => Piece { color: Color::White, kind: PieceKind::King },
-            'p' => Piece { color: Color::Black, kind: PieceKind::Pawn },
-            'n' => Piece { color: Color::Black, kind: PieceKind::Knight },
-            'b' => Piece { color: Color::Black, kind: PieceKind::Bishop },
-            'r' => Piece { color: Color::Black, kind: PieceKind::Rook },
-            'q' => Piece { color: Color::Black, kind: PieceKind::Queen },
-            'k' => Piece { color: Color::Black, kind: PieceKind::King },
-            _ => panic!(),
+        if c.is_ascii_lowercase() {
+            Piece {
+                color: Color::Black,
+                kind: PieceKind::from_char(c),
+            }
+        } else {
+            Piece {
+                color: Color::White,
+                kind: PieceKind::from_char(c.to_ascii_lowercase()),
+            }
         }
     }
 
     fn to_char(self) -> char {
-        match self {
-            Piece { color: Color::White, kind: PieceKind::Pawn } => 'P',
-            Piece { color: Color::White, kind: PieceKind::Knight } => 'N',
-            Piece { color: Color::White, kind: PieceKind::Bishop } => 'B',
-            Piece { color: Color::White, kind: PieceKind::Rook } => 'R',
-            Piece { color: Color::White, kind: PieceKind::Queen } => 'Q',
-            Piece { color: Color::White, kind: PieceKind::King } => 'K',
-            Piece { color: Color::Black, kind: PieceKind::Pawn } => 'p',
-            Piece { color: Color::Black, kind: PieceKind::Knight } => 'n',
-            Piece { color: Color::Black, kind: PieceKind::Bishop } => 'b',
-            Piece { color: Color::Black, kind: PieceKind::Rook } => 'r',
-            Piece { color: Color::Black, kind: PieceKind::Queen } => 'q',
-            Piece { color: Color::Black, kind: PieceKind::King } => 'k',
+        match self.color {
+            Color::Black => self.kind.to_char(),
+            Color::White => self.kind.to_char().to_ascii_uppercase(),
         }
     }
 }
@@ -66,5 +79,64 @@ impl Piece {
 impl std::fmt::Debug for Piece {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "<{}>", self.to_char())
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Move {
+    pub from: i32,
+    pub to: i32,
+    pub promotion: Option<PieceKind>,
+}
+
+impl Move {
+    pub fn from_uci(s: &str) -> Move {
+        let p = if s.len() == 5 {
+            Some(s.chars().rev().next().unwrap())
+        } else if s.len() == 4 {
+            None
+        } else {
+            panic!("{:?}", s)
+        };
+        Move {
+            from: square_from_uci(&s[..2]),
+            to: square_from_uci(&s[2..4]),
+            promotion: p.map(PieceKind::from_char),
+        }
+    }
+}
+
+fn square_from_uci(s: &str) -> i32 {
+    let mut it = s.chars();
+    let file = it.next().unwrap();
+    let rank = it.next().unwrap();
+    assert!(it.next().is_none());
+    assert!('a' <= file && file <= 'h');
+    assert!('1' <= rank && rank <= '8');
+    (file as i32 - 'a' as i32) + 8 * (rank as i32 - '1' as i32)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_piece_to_from_char() {
+        for c in "pnbrqkPNBRQK".chars() {
+            assert_eq!(Piece::from_char(c).to_char(), c);
+        }
+    }
+
+    #[test]
+    fn test_square_from_uci() {
+        assert_eq!(square_from_uci("a1"), 0);
+        assert_eq!(square_from_uci("b1"), 1);
+        assert_eq!(square_from_uci("h8"), 63);
+    }
+
+    #[test]
+    fn test_move_from_uci() {
+        assert_eq!(Move::from_uci("a2c1"), Move { from: 8, to: 2, promotion: None });
+        assert_eq!(Move::from_uci("a2c1q"), Move { from: 8, to: 2, promotion: Some(PieceKind::Queen) });
     }
 }
