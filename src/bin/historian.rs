4bb31b8c2f87bc1e5da93aefbff41d1bc80b54f6
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2019-09-01 20:08:35 +0300

    Improve diagnostics in historian

--- src/bin/historian.rs
+++ src/bin/historian.rs
@@ -1,7 +1,29 @@
 use std::io::Read;
+use std::sync::Mutex;
 use rusqlite::{Connection, params};
 use rbc::api;
 use rbc::game::{STARTING_FEN, BoardState, Move};
+
+fn check_game(h: api::GameHistory, log: &Mutex<Vec<String>>) {
+    for (i, m) in h.moves.iter().enumerate() {
+        if i == 0 {
+            assert_eq!(m.fen_before, STARTING_FEN);
+        } else {
+            assert_eq!(m.fen_before, h.moves[i - 1].fen_after);
+        }
+        let before: BoardState = fen::BoardState::from_fen(&m.fen_before).unwrap().into();
+        let after: BoardState = fen::BoardState::from_fen(&m.fen_after).unwrap().into();
+        if let Some(q) = &m.requested_move {
+            Move::from_uci(q);
+        }
+        let mut state = before;
+        log.lock().unwrap().push(m.taken_move.clone().unwrap_or("--".into()));
+        let m = m.taken_move.as_ref().map(|s| Move::from_uci(s));
+        state.make_move(m);
+        state.en_passant_square = after.en_passant_square;  // TODO
+        assert_eq!(state, after);
+    }
+}
 
 fn main() {
     env_logger::init();
@@ -19,9 +41,7 @@ fn main() {
 
     conn.prepare("
         SELECT game_id, dict_id, data
-        FROM game
-        --WHERE game_id > 15900
-        LIMIT 100000").unwrap()
+        FROM game").unwrap()
     .query_map(params![], |row| {
         let game_id: i32 = row.get(0)?;
         let dict_id: i64 = row.get(1)?;
@@ -38,31 +58,24 @@ fn main() {
         let mut h = String::new();
         dec.read_to_string(&mut h).unwrap();
 
-
-        if game_id % 10 == 0 {
-            println!("{}    {} {}", game_id, data.len(), h.len());
+        if game_id % 100 == 0 {
+            println!("{}", game_id);
         }
 
         let h: api::GameHistoryResponse = serde_json::from_str(&h).unwrap();
         let h: api::GameHistory = h.game_history.into();
-        // dbg!(game_id);
-        for (i, m) in h.moves.iter().enumerate() {
-            if i == 0 {
-                assert_eq!(m.fen_before, STARTING_FEN);
-            } else {
-                assert_eq!(m.fen_before, h.moves[i - 1].fen_after);
+
+        let log = Mutex::new(Vec::new());
+        match std::panic::catch_unwind(|| { check_game(h, &log); }) {
+            Ok(()) => {},
+            Err(e) => {
+                dbg!(game_id);
+                dbg!(e);
+                for m in log.into_inner().unwrap() {
+                    println!("{}", m);
+                }
+                std::process::exit(1);
             }
-            let before: BoardState = fen::BoardState::from_fen(&m.fen_before).unwrap().into();
-            let after: BoardState = fen::BoardState::from_fen(&m.fen_after).unwrap().into();
-            if let Some(q) = &m.requested_move {
-                Move::from_uci(q);
-            }
-            let mut state = before;
-            // dbg!(m.taken_move.as_ref().unwrap_or(&"--".into()));
-            let m = m.taken_move.as_ref().map(|s| Move::from_uci(s));
-            state.make_move(m);
-            state.en_passant_square = after.en_passant_square;  // TODO
-            assert_eq!(state, after);
         }
 
         Ok(())
